fc6d91b3b532f95a648961863f7be69f508018c7
vladyslav <vladpavlishchuk@gmail.com>
2019-07-02 14:58:15 +0300

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :category
+
+  validates :name, presence: true, length: { maximum: 120 }
+  validates :price, presence: true, if: Proc.new{ |p| p.price > 0 && p.price < 1000000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_one :category
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
